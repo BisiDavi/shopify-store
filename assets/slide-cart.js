c7916dd2770fb0ef717d0a58ec203939860435c8
BisiDavi <oludavidconnect@gmail.com>
2022-12-04 20:34:05 +0100

    slider updated

--- assets/slide-cart.js
+++ assets/slide-cart.js
@@ -3,11 +3,45 @@ class SlideCart extends HTMLElement {
     super();
 
     this.slider = document.getElementById("cart-slider");
-    this.onBodyClick = this.handleBodyClick.bind(this);
+    this.closeButton = document.getElementById("closeButton");
+    this.openButton = document.getElementById("cart-icon-bubble");
 
     this.notification.addEventListener(
       "keyup",
       (evt) => evt.code === "Escape" && this.close()
     );
+    this.closeButton.addEventListener("click", this.close.bind(this));
+    this.openButton.addEventListener("click", this.open.bind(this));
+  }
+
+  open() {
+    this.slider.classList.add("active");
+
+    this.slider.addEventListener(
+      "transitionend",
+      () => {
+        this.slider.focus();
+        trapFocus(this.slider);
+      },
+      { once: true }
+    );
+  }
+
+  close() {
+    this.slider.classList.remove("active");
+
+    removeTrapFocus(this.activeElement);
+  }
+
+  getSectionInnerHTML(html, selector = ".shopify-section") {
+    return new DOMParser()
+      .parseFromString(html, "text/html")
+      .querySelector(selector).innerHTML;
+  }
+
+  setActiveElement(element) {
+    this.activeElement = element;
   }
 }
+
+customElements.define("slide-cart", SlideCart);
